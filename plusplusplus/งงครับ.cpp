b58ef7b7778ab0820e990c275cf7364bf953c28a
PEERAPAT SATTAPORNNARA <63010702@KMITL.AC.TH>
2020-08-30 23:47:45 +0700

    เเก้่ไข

--- plusplusplus/งงครับ.cpp
+++ plusplusplus/งงครับ.cpp
@@ -1,10 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-
+#include<math.h>
 
 int main()
 {
-	int a, c = 0;
+	int a, c = 0, d = 0;
 	printf("Enter Number : ");
 	scanf("%d", &a);
 	while (a >= 10)
@@ -13,8 +13,15 @@ int main()
 		a = (a - (a % 10)) / 10;
 	}
 	c = c + a;
-	printf("%d", c);
+	printf("%d\n", c);
+
+	if (c > 10)
+	{
+		d += c % 10;
+		c = (c - (d)) / 10;
+		c = c + d;
+		printf("%d ", c);
+	}
+
 	return 0;
-
 }
-
